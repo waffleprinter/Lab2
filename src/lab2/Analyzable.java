74dbdc204ddd574ecfee0b65b74f1f2211f9c295
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-08-29 13:50:03 -0400

    initial commit

---
+++ src/lab2/Analyzable.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package lab2;
+
+/**
+ *
+ * @author 6298805
+ */
+public interface Analyzable {
+    public double getAverage();
+    public double getHighest();
+    public double getLowest();
+}
