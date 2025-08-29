b22951c9e2a679d83a745559ba6b36f202932665
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-08-29 13:42:34 -0400

    initial commit

--- src/lab2/CourseGrades.java
+++ src/lab2/CourseGrades.java
@@ -9,5 +9,33 @@ package lab2;
  * @author 6298805
  */
 public class CourseGrades {
+    private GradedActivity[] grades = new GradedActivity[4];
+    private final int NUM_GRADES = 4;
     
+    public CourseGrades() {
+        
+    }
+    
+    public void setLab(GradedActivity lab) {
+        grades[0] = lab;
+    }
+    
+    public void setPassFailExam(PassFailExam passFailExam) {
+        grades[1] = passFailExam;
+    }
+    
+    public void setEssay(Essay essay) {
+        grades[2] = essay;
+    }
+    
+    public void setFinalExam(FinalExam finalExam) {
+        grades[3] = finalExam;
+    }
+    
+    public String toString() {
+        return "Lab score " + grades[0].getScore() + " Grade: " + grades[0].getGrade() +
+                "\nPass/fail exam score: " + grades[1].getScore() + " Grade: " + grades[1].getGrade() + 
+                "\nEssay score " + grades[2].getScore() + " Grade: " + grades[2].getGrade() +
+                "\nFinal exam score " + grades[3].getScore() + " Grade: " + grades[3].getGrade();
+    }
 }
