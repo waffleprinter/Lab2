9f5b210633204332e403a9b4b3567e7b68108b2c
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-08-29 12:41:31 -0400

    added setscore, getscore, getgrade

--- src/lab2/GradedActivity.java
+++ src/lab2/GradedActivity.java
@@ -9,5 +9,27 @@ package lab2;
  * @author 6298805
  */
 public class GradedActivity {
+    private double score;
     
+    public void setScore(double score) {
+        this.score = score;
+    }
+    
+    public double getScore() {
+        return this.score;
+    }
+    
+    public char getGrade() {
+        if (score >= 90) {
+            return 'A';
+        } else if (score >= 80) {
+            return 'B';
+        } else if (score >= 70) {
+            return 'c';
+        } else if (score >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
 }
