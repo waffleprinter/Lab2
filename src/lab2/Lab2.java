c85799990b725b203833d0384b18f267d0880056
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-08-29 12:55:00 -0400

    added test code

--- src/lab2/Lab2.java
+++ src/lab2/Lab2.java
@@ -14,7 +14,13 @@ public class Lab2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        FinalExam finalExam = new FinalExam(100, 25);
+        
+        System.out.println("How many questions are on the final exam? 100");
+        System.out.println("How many questions are on the final exam? 25");
+        System.out.printf("Each question counts %.1f points%n", finalExam.getPointsEach());
+        System.out.printf("The exam score is %.1f%n", finalExam.getScore());
+        System.out.printf("The exam grade is %s%n", finalExam.getGrade());
     }
     
 }
