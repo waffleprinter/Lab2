2cc6b0b03a3af89e47799f2d30be6bb723e5e73b
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-08-29 13:41:56 -0400

    added teacher-given testing code

--- src/lab2/Lab2.java
+++ src/lab2/Lab2.java
@@ -4,9 +4,11 @@
  */
 package lab2;
 
+import java.util.*;
+
 /**
  *
- * @author 6298805
+ * @author Nagat
  */
 public class Lab2 {
 
@@ -14,13 +16,50 @@ public class Lab2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        FinalExam finalExam = new FinalExam(100, 25);
+        // TODO code application logic here
+        System.out.println("Step 2: ");
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many quetions are on the final exam? ");
+        int questions = input.nextInt();
+        System.out.print("How many questions did the student miss? ");
+        int missed = input.nextInt();
+        System.out.println(new FinalExam(questions,missed));
         
-        System.out.println("How many questions are on the final exam? 100");
-        System.out.println("How many questions are on the final exam? 25");
-        System.out.printf("Each question counts %.1f points%n", finalExam.getPointsEach());
-        System.out.printf("The exam score is %.1f%n", finalExam.getScore());
-        System.out.printf("The exam grade is %s%n", finalExam.getGrade());
+        System.out.println("\nStep 3: ");
+        System.out.print("How many quetions are on the final exam? ");
+        questions = input.nextInt();
+        System.out.print("How many questions did the student miss? ");
+        missed = input.nextInt();
+        System.out.print("What is the minimum passing score? ");
+        double minPassingScore = input.nextInt();
+        System.out.println(new PassFailExam(questions,missed,minPassingScore));
+        
+        System.out.println("\nStep 4: ");
+        System.out.println("Term Paper:");
+        System.out.print("Grammar points: ");
+        double grammar = input.nextDouble();
+        System.out.print("Spelling points: ");
+        double spelling = input.nextDouble();
+        System.out.print("Length points: ");
+        double length = input.nextDouble();
+        System.out.print("Content points: ");
+        double content = input.nextDouble();
+        Essay e1 = new Essay();
+        e1.setScore(grammar, spelling, length, content);
+        System.out.println(e1.toString());
+        
+        System.out.println("\nStep 5:");
+        CourseGrades cG = new CourseGrades();
+        GradedActivity gA = new GradedActivity();
+        gA.setScore(85);
+        cG.setLab(gA);
+        PassFailExam pFE = new PassFailExam(10,1,70);
+        cG.setPassFailExam(pFE);
+        Essay e2 = new Essay();
+        e2.setScore(10,20,20,20);
+        cG.setEssay(e2);
+        FinalExam fE = new FinalExam(50,1);
+        cG.setFinalExam(fE);
+        System.out.println(cG);
     }
-    
 }
