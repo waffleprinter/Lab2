6fca67f98ceb15eb28945afae9bc3008cc384b8e
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-08-29 13:06:13 -0400

    Initial commit, variables and getters and setters

---
+++ src/lab2/Essay.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab2;
+
+/**
+ *
+ * @author 6298805
+ */
+public class Essay extends GradedActivity {
+    private double grammar;
+    private double spelling;
+    private double correctLength;
+    private double content;
+
+    public double getGrammar() {
+        return grammar;
+    }
+
+    public void setGrammar(double grammar) {
+        this.grammar = grammar;
+    }
+
+    public double getSpelling() {
+        return spelling;
+    }
+
+    public void setSpelling(double spelling) {
+        this.spelling = spelling;
+    }
+
+    public double getCorrectLength() {
+        return correctLength;
+    }
+
+    public void setCorrectLength(double correctLength) {
+        this.correctLength = correctLength;
+    }
+
+    public double getContent() {
+        return content;
+    }
+
+    public void setContent(double content) {
+        this.content = content;
+    }
+    
+    
+}
